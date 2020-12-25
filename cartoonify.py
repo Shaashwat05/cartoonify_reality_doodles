c9684cd22fadf531fa17515f71b5c94007f71aa1
Shaashwat <shaas2000@gmail.com>
2020-12-25 16:49:20 +0530

    position and size fix

--- cartoonify.py
+++ cartoonify.py
@@ -1,14 +1,13 @@
 import cv2
 from yolo import yolo
-from draw import get_objects, drawing, position
+from draw import get_objects, drawing
 
 # Object Detection
 image = cv2.imread("gallery/image1.png")
 boxes, classes = yolo(image)
 
 # Objects -> doodles
-objs = get_objects(classes)
-
-drawing(objs)
+boxes_final, objs = get_objects(boxes, classes)
+drawing(boxes_final, objs)
 
 
--- draw.py
+++ draw.py
@@ -2,6 +2,8 @@ import struct
 from struct import unpack
 import numpy as np
 import gizeh as gz
+from os import path
+
 
 def unpack_drawing(file_handle):
     key_id, = unpack('Q', file_handle.read(8))
@@ -36,49 +38,58 @@ def unpack_drawings(filename):
 
 
 
-def get_objects(classes):
+def get_objects(boxes, classes):
 
     objs = []
     LABELS =open("yolo/coco.names").read().strip().split("\n")
+    boxes2 = []
 
+    i=-1
     for y_class in classes:
-
+        i+=1
         try:
-            #print('dataset/'+str(LABELS[y_class])+'.bin')
             arr = unpack_drawings('dataset/'+str(LABELS[y_class])+'.bin')
-            
+            if(path.exists('dataset/'+str(LABELS[y_class])+'.bin')):
+                boxes2.append(boxes[i])
             num = np.random.randint(0,50)
             for drawing in arr:
                 objs.append(drawing)
                 break
-
         except:
             continue
     
-    return objs
+    return boxes2, objs
 
 
-def drawing(objs):
+def drawing(boxes, objs):
 
     surface = gz.Surface(width=720, height=560) # in pixels
     rect = gz.rectangle(lx=720, ly=560, xy=(360,280), fill=(1,1,1))
     rect.draw(surface)
 
+    i=0
     for strokes in objs:
         lines_list = []
         for stroke in strokes['image']:
             x, y = stroke
+
+            x = tuple([(z*boxes[i][2])//255 for z in x])
+            y = tuple([(z*boxes[i][3])//255 for z in y])
+
+            x = tuple([z+boxes[i][0] for z in x])
+            y = tuple([z+boxes[i][1] for z in y])
+
             points = list(zip(x, y))
             line = gz.polyline(points=points, stroke=[0,0,0], stroke_width=2)
             lines_list.append(line)
 
-
         lines = gz.Group(lines_list)
         lines.draw(surface)
+        i+=1
 
-    surface.write_to_png("circle.png")
+    surface.write_to_png("gallery/circle.png")
 
 
 
-def position():
-    pass
+    
+    
